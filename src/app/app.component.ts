f107204503c2ffbbc190da8448a2baf2c5fbc921
Elliot Wilkinson <elliotwilkinson@elliots-mbp.home>
2017-09-12 21:49:04 +0100

    Implemented g f e and x

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  testValues = [0, 1000, -1000, 500.555, 1.000001, 1.000005, 0.682287199917446785, "0.682287199917446785"];
+  testValues = [0, 1000, -1000, 500.555, 0.000001, 1.000005, 0.682287199917446785, "0.682287199917446785"];
   testPrecisionSpecifiers = [0, 2, 5];
-  testFormatSpecifiers = ['r']; //['n', 'c', 'p', 'd'];
+  testFormatSpecifiers = ['g'];//['x', 'X', 'n', 'c', 'p', 'd'];
 }
--- src/app/pipes/format.pipe.ts
+++ src/app/pipes/format.pipe.ts
@@ -21,10 +21,19 @@ export class FormatPipe implements PipeTransform {
   numericTransform(value: string | number, format: string): string {
     let formatSpecifier = format[0];
     let precisionSpecifier = Math.min(20, +format.slice(1));
-    let formattedValue = value.toString();    
+    let formattedValue = value.toString();
 
     switch (formatSpecifier.toLowerCase()) {
-      case 'n':
+      case 'g':
+        let exponentFormatValue = this.numericTransform(value, 'e'+precisionSpecifier);
+        let exponentValue = +exponentFormatValue.substr(exponentFormatValue.indexOf('E'));
+        if(exponentValue > -5 && exponentValue < precisionSpecifier){
+            formattedValue = (+(+value).toPrecision(precisionSpecifier)).toString();
+            break;
+        }
+        formattedValue = formatSpecifier == 'g' ? exponentFormatValue.toString() : exponentFormatValue.toString().toUpperCase();        
+        break;
+      case 'f':
         formattedValue = (+value).toFixed(precisionSpecifier);
         break;
       case 'p':
@@ -33,21 +42,37 @@ export class FormatPipe implements PipeTransform {
       case 'c':
         console.warn("C format not yet supported");
         break;
+      case 'e':
+        formattedValue = (+value).toExponential(precisionSpecifier);
+        break;  
       case 'd':
-        if(!Number.isInteger(+value)){
+        if (!Number.isInteger(+value)) {
           console.warn("Value " + value + " is not of integral type. The Decimal format specifier (d) only supports integral types.")
           break;
         }
         let isNegative = value < 0;
         let positiveDecimalValue = Math.abs(+value);
-        let lengthToAdd = Math.max(0, precisionSpecifier - positiveDecimalValue.toString().length);
+        var lengthToAdd = Math.max(0, precisionSpecifier - positiveDecimalValue.toString().length);
         formattedValue = (isNegative ? '-' : '') + '0'.repeat(lengthToAdd) + positiveDecimalValue.toString();
       case 'r':
-        if(typeof (value) == "number"){
+        if (typeof (value) == "number") {
           formattedValue = value.toString();
           break;
-        } 
+        }
         formattedValue = Number(value).toString();
+      case 'x':
+        if (!Number.isInteger(+value)) {
+          console.warn("Value " + value + " is not of integral type. The Hex format specifier (x|X) only supports integral types.")
+          break;
+        }
+
+        formattedValue = (+value < 0 ? (0xFFFFFFFF + +value + 1) : +value).toString(16);
+
+        lengthToAdd = Math.max(0, precisionSpecifier - formattedValue.toString().length);
+        formattedValue = '0'.repeat(lengthToAdd) + formattedValue;
+
+        if(formatSpecifier == 'X') formattedValue = formattedValue.toUpperCase();
+        break;
       default:
         console.warn("No format found corresponding to the format specifier: " + formatSpecifier);
         break;
@@ -56,5 +81,4 @@ export class FormatPipe implements PipeTransform {
     return formattedValue;
 
   }
-
 }
